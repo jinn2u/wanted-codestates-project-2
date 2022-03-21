8b2b45e50d6c86e41e822923a920ed373f44d89a
jinn2u <bsybear623@gmail.com>
2022-03-21 14:07:13 +0900

    feat: index Page 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 
 reportWebVitals();
