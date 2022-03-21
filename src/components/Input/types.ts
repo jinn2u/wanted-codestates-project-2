2331b1520f42d66819300a974dbb830e75a1624f
jinn2u <bsybear623@gmail.com>
2022-03-21 16:52:28 +0900

    feat: input component markup

---
+++ src/components/Input/types.ts
@@ -0,0 +1,11 @@
+export interface Props {
+  width?: number;
+  height?: number;
+  borderColor?: string;
+  placeholder?: string;
+}
+export interface IInp {
+  width: number;
+  height: number;
+  borderColor: string;
+}
