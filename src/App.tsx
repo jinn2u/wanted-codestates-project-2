13a1412beb1cdbbd0230b8748f795c6d059c5646
jinn2u <bsybear623@gmail.com>
2022-03-21 16:51:11 +0900

    feat: themeProvider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Global, ThemeProvider } from '@emotion/react';
 import Router from './routes/Router';
+import reset from './styles/reset';
+import { theme } from './styles/theme';
 
 const App = () => {
-  return <Router />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={reset} />
+      <Router />
+    </ThemeProvider>
+  );
 };
 export default App;
