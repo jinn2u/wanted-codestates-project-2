f2903b21eaa100bb4616fef5c2878dd852b8cc9e
jinn2u <bsybear623@gmail.com>
2022-03-22 17:14:37 +0900

    feat: 절대 경로 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { store } from './redux/index';
 
 ReactDOM.render(
@@ -13,5 +12,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root'),
 );
-
-reportWebVitals();
