c66578f029ab59182ffcf404e763db697bcef802
jinn2u <bsybear623@gmail.com>
2022-03-23 13:04:35 +0900

    feat: Header component markup

---
+++ src/Pages/UserPage/components/Header/style.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Head = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1000px;
+  height: 48px;
+  padding: 0 10px;
+  margin: 0 auto;
+`;
+export const Right = styled.h2`
+  font-size: 10px;
+  color: #6c7a89;
+`;
